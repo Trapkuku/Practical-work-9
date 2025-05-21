734d8ed59343ed1d60eb949d66cf4beba097c5fe
Savelii Balabanov <134796764+Trapkuku@users.noreply.github.com>
2025-05-21 21:04:53 +0200

    backend_rooms.php

--- backend_rooms.php
+++ backend_rooms.php
@@ -1,22 +1,22 @@
 <?php
 require_once '_db.php';
 
-$stmt = $db->prepare("SELECT * FROM rooms ORDER BY name");
+$stmt = $db->prepare("SELECT * FROM rooms");
 $stmt->execute();
-$rooms = $stmt->fetchAll();
+$rooms = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 class Room {}
-$result = array();
+$roomList = array();
 
-foreach($rooms as $room) {
-    $r = new Room();
-    $r->id = $room['id'];
-    $r->name = $room['name'];
-    $r->capacity = $room['capacity'];
-    $r->status = $room['status'];
-    $result[] = $r;
+foreach($rooms as $r) {
+    $room = new Room();
+    $room->id = $r['id'];
+    $room->name = $r['name'];
+    $room->capacity = $r['capacity'];
+    $room->status = $r['status'];
+    $roomList[] = $room;
 }
 
 header('Content-Type: application/json');
-echo json_encode($result);
+echo json_encode($roomList);
 ?>
