231eff1b4f7b444d99952e66f54431b145620cb6
Savelii Balabanov <134796764+Trapkuku@users.noreply.github.com>
2025-05-21 21:09:34 +0200

    Update _db.php

--- _db.php
+++ _db.php
@@ -1,18 +1,14 @@
 <?php
-$host = getenv('MYSQLHOST');
-$port = getenv('MYSQLPORT');
-$username = getenv('MYSQLUSER');
-$password = getenv('MYSQLPASSWORD');
-$database = getenv('MYSQLDATABASE');
+$host = "shortline.proxy.rlwy.net";
+$port = 58433;
+$username = "root";
+$password = "OyDNrxEeeGyzZqmjEcdOtcTpXUvcVZrA";
+$database = "railway";
 
 try {
-    $db = new PDO(
-        "mysql:host=$host;port=$port;dbname=$database;charset=utf8mb4",
-        $username,
-        $password
-    );
+    $db = new PDO("mysql:host=$host;port=$port;dbname=$database;charset=utf8", $username, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $ex) {
-    die("Database connection failed: " . $ex->getMessage());
+} catch (PDOException $e) {
+    die("Database connection failed: " . $e->getMessage());
 }
 ?>
