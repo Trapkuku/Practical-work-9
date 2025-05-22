a6a028d929cf0c44576ae001f164c6b46c758555
Savelii Balabanov <134796764+Trapkuku@users.noreply.github.com>
2025-05-22 17:33:27 +0200

    _db.php

--- _db.php
+++ _db.php
@@ -1,16 +1,14 @@
 <?php
-// Railway connection parameters (проверь их в интерфейсе Railway!)
-$host = "mysql.railway.internal";   // или host из Railway
+$host = "mysql.railway.internal";        // это внутренний адрес для Railway
 $port = 3306;
 $username = "root";
-$password = "aQmCyKNKWKAKEQNdUEkCjshVXbEoULgd"; // возьми пароль из Railway MYSQLPASSWORD
-$database = "railway";             // имя базы тоже возьми из Railway MYSQLDATABASE
+$password = "aQmCyKNKWKAKEQNdUEkCjshVXbEoULgd"; // возьми свой из VARIABLES -> MYSQLPASSWORD
+$database = "railway";                   // или свое имя БД
 
 try {
     $db = new PDO("mysql:host=$host;port=$port;dbname=$database", $username, $password);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    // Выведет ошибку на экран (в продакшене — лучше писать в лог)
     die("Database connection failed: " . $e->getMessage());
 }
 ?>
