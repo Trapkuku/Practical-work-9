4cba3e76af79ba0ef0d87f69dde14894b7d6848f
Savelii Balabanov <134796764+Trapkuku@users.noreply.github.com>
2025-05-23 00:07:26 +0200

    Update backend_move.php

--- backend_move.php
+++ backend_move.php
@@ -1,7 +1,11 @@
 <?php
 require_once '_db.php';
 
-class Result {}
+class Result {
+    public $result;
+    public $message;
+}
+
 
 $stmt = $db->prepare("SELECT * FROM reservations WHERE NOT ((end <= :start) OR (start >= :end)) AND id <> :id AND room_id = :resource");
 $stmt->bindParam(':start', $_POST['newStart']);
@@ -11,12 +15,11 @@ $stmt->bindParam(':resource', $_POST['newResource']);
 $stmt->execute();
 $overlaps = $stmt->rowCount() > 0;
 
+header('Content-Type: application/json');
 if ($overlaps) {
     $response = new Result();
     $response->result = 'Error';
     $response->message = 'This reservation overlaps with an existing reservation.';
-
-    header('Content-Type: application/json');
     echo json_encode($response);
     exit;
 }
@@ -31,8 +34,5 @@ $stmt->execute();
 $response = new Result();
 $response->result = 'OK';
 $response->message = 'Update successful';
-
-header('Content-Type: application/json');
 echo json_encode($response);
-exit;
 ?>
