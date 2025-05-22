4f076106b22ea1ff70b31247488a2063d243b01f
Savelii Balabanov <134796764+Trapkuku@users.noreply.github.com>
2025-05-22 18:22:58 +0200

    Update backend_events.php

--- backend_events.php
+++ backend_events.php
@@ -5,25 +5,22 @@ $stmt = $db->prepare("SELECT * FROM reservations");
 $stmt->execute();
 $result = $stmt->fetchAll();
 
-class Event {}
 $events = array();
 
 foreach($result as $row) {
-    if (empty($row['name']) || empty($row['start']) || empty($row['end']) || empty($row['room_id'])) {
-        continue;
-    }
-    $e = new Event();
-    $e->id = $row['id'];
-    $e->text = $row['name'];
-    $e->start = $row['start'];
-    $e->end = $row['end'];
-    $e->resource = $row['room_id'];
-    $e->status = $row['status'];
-    $e->paid = $row['paid'];
-    $e->bubbleHtml = "Reservation details:<br/>".$e->text;
-    $events[] = $e;
+    $events[] = array(
+        "id" => $row['id'],
+        "text" => $row['name'],
+        "start" => $row['start'],
+        "end" => $row['end'],
+        "resource" => $row['room_id'],
+        "status" => $row['status'],
+        "paid" => $row['paid'],
+        "bubbleHtml" => "Reservation details:" . $row['name']
+    );
 }
 
 header('Content-Type: application/json');
 echo json_encode($events);
+exit;
 ?>
